b825797ab1f73db182d1c2d196d3508dd3d9836c
Houssan Ali Hijazi <hussanhijazi@gmail.com>
2014-05-26 13:33:00 -0300

    V1.0

---
+++ src/org/desaparecidosbr/app/api/PessoasApi.java
@@ -0,0 +1,61 @@
+package org.desaparecidosbr.app.api;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.desaparecidosbr.app.entity.Pessoa;
+import org.json.JSONArray;
+
+import android.app.Activity;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+public class PessoasApi extends ApiCore {
+
+    private Gson gson;
+
+    public PessoasApi(Activity activity, ApiListener apiListener) {
+        super(activity, apiListener);
+    }
+
+    @Override
+    public String getUrl(HashMap<String, String> parameters) {
+        return createUrl(ApiCore.BASE_URL + "pessoas", parameters);
+    }
+
+    @Override
+    public void handleJsonResponse(int operation, JSONArray array) {
+
+        ArrayList<Pessoa> arrayList = new ArrayList<Pessoa>();
+        Type collectionType = new TypeToken<ArrayList<Pessoa>>() {
+        }.getType();
+        gson = new GsonBuilder().create();
+        try {
+            
+          //  int nObjs = array.length();
+            arrayList = gson.fromJson(array.toString(), collectionType);
+            //for (int i = 0; i < nObjs; i++) {
+                
+              //  Tweet tweet = gson.fromJson(object.toString(), Tweet.class);
+//              arrayList = gson.fromJson(array.toString(), collectionType);
+
+//                lojasAdapter = new PessoasAdapter(context, lojasList);
+//                // lojasAdapter.setItens(lojasList);
+//                lojasListView.setAdapter(lojasAdapter);
+//                
+//                arrayList.add(tweet);
+         //   }
+
+            if (mApiListener != null) {
+                mApiListener.onRequestEnds(true, arrayList);
+            }
+        } catch (Exception e) {
+            if (mApiListener != null) {
+                mApiListener.onRequestEnds( false, arrayList);
+            }
+        }
+    }
+}
---
+++ src/org/desaparecidosbr/app/utils/Utility.java
@@ -0,0 +1,238 @@
+
+package org.desaparecidosbr.app.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.android.volley.toolbox.NetworkImageView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class Utility {
+    // public static void setListViewHeightBasedOnChildren(ListView listView) {
+    // ListAdapter listAdapter = listView.getAdapter();
+    // if (listAdapter == null) {
+    // // pre-condition
+    // return;
+    // }
+    //
+    // int totalHeight = 0;
+    // int desiredWidth = MeasureSpec.makeMeasureSpec(listView.getWidth(),
+    // MeasureSpec.AT_MOST);
+    // for (int i = 0; i < listAdapter.getCount(); i++) {
+    // View listItem = listAdapter.getView(i, null, listView);
+    // listItem.measure(desiredWidth, MeasureSpec.UNSPECIFIED);
+    // totalHeight += listItem.getMeasuredHeight();
+    // }
+    //
+    // ViewGroup.LayoutParams params = listView.getLayoutParams();
+    // params.height = totalHeight + (listView.getDividerHeight() *
+    // (listAdapter.getCount() - 1));
+    // listView.setLayoutParams(params);
+    // listView.requestLayout();
+    // }
+    public static void setListViewSize2(ListView myListView)
+    {
+        
+        
+        ListAdapter myListAdapter = myListView.getAdapter();
+        if (myListAdapter == null) {
+            //do nothing return null
+            return;
+        }
+        //set listAdapter in loop for getting final size
+        int totalHeight = 0;
+        for (int size = 0; size < myListAdapter.getCount(); size++) {
+            View listItem = myListAdapter.getView(size, null, myListView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+      //setting listview item in adapter
+        ViewGroup.LayoutParams params = myListView.getLayoutParams();
+        params.height = totalHeight + (myListView.getDividerHeight() * (myListAdapter.getCount() - 1));
+        myListView.setLayoutParams(params);
+        // print height of adapter on log
+       // Log.i("height of listItem:", String.valueOf(totalHeight));
+
+
+    }
+    public static void setListViewSize(ListView myListView) {
+        ListAdapter myListAdapter = myListView.getAdapter();
+        if (myListAdapter == null) {
+            // do nothing return null
+            return;
+        }
+        // set listAdapter in loop for getting final size
+        int totalHeight = 0;
+        for (int size = 0; size < myListAdapter.getCount(); size++) {
+            View listItem = myListAdapter.getView(size, null, myListView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight() + listItem.getMeasuredHeight() / 30;
+        }
+        // setting listview item in adapter
+        ViewGroup.LayoutParams params = myListView.getLayoutParams();
+        params.height = totalHeight
+                + (myListView.getDividerHeight() * (myListAdapter.getCount() - 1));
+        myListView.setLayoutParams(params);
+        // print height of adapter on log
+    }
+
+    public static String formatPrice(Double preco) {
+        return new DecimalFormat("#.00").format(preco);
+    }
+
+    public static Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+        // RECREATE THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+        return resizedBitmap;
+
+    }
+
+//    public static String hmacSha1(String message, String secret) throws NoSuchAlgorithmException,
+//            InvalidKeyException {
+//        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+//        SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+//        sha256_HMAC.init(secret_key);
+//        String hash = Base64.encodeBase64String(sha256_HMAC.doFinal(message.getBytes()));
+//
+//        return hash;
+//    }
+
+    public static String hmacDigest(String msg, String keyString) {
+        String digest = null;
+        try {
+            SecretKeySpec key = null;
+            try {
+                key = new SecretKeySpec((keyString).getBytes("UTF-8"), "HmacSHA256");
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(key);
+
+            byte[] bytes = null;
+            try {
+                bytes = mac.doFinal(msg.getBytes("ASCII"));
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            StringBuffer hash = new StringBuffer();
+            for (int i = 0; i < bytes.length; i++) {
+                String hex = Integer.toHexString(0xFF & bytes[i]);
+                if (hex.length() == 1) {
+                    hash.append('0');
+                }
+                hash.append(hex);
+            }
+            digest = hash.toString();
+        } catch (InvalidKeyException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+        return digest;
+    }
+    public static void addAds(AdView adView)
+    {
+        //AdView adView = (AdView) this.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+    }
+    public static String formatData(String data)
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        SimpleDateFormat formatter2 = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+        Date date = new Date();
+        try {
+            date = formatter.parse(data);
+            
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return(formatter2.format(date).toString());
+    }
+
+    public static final boolean checkInternetConnection(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isAvailable() && cm.getActiveNetworkInfo().isConnected()) {
+          return true;
+
+        } else {
+          return false;
+        }
+      }
+    public static final  void scaleImage(NetworkImageView view, int boundBoxInDp)
+    {
+        // Get the ImageView and its bitmap
+        Drawable drawing = view.getDrawable();
+        Bitmap bitmap = ((BitmapDrawable)drawing).getBitmap();
+
+        // Get current dimensions
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        // Determine how much to scale: the dimension requiring less scaling is
+        // closer to the its side. This way the image always stays inside your
+        // bounding box AND either x/y axis touches it.
+        float xScale = ((float) boundBoxInDp) / width;
+        float yScale = ((float) boundBoxInDp) / height;
+        float scale = (xScale <= yScale) ? xScale : yScale;
+
+        // Create a matrix for the scaling and add the scaling data
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);
+
+        // Create a new bitmap and convert it to a format understood by the ImageView
+        Bitmap scaledBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+        BitmapDrawable result = new BitmapDrawable(scaledBitmap);
+        width = scaledBitmap.getWidth();
+        height = scaledBitmap.getHeight();
+
+        // Apply the scaled bitmap
+        view.setImageDrawable(result);
+
+        // Now change ImageView's dimensions to match the scaled image
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) view.getLayoutParams();
+        params.width = width;
+        params.height = height;
+        view.setLayoutParams(params);
+    }
+
+ 
+}
